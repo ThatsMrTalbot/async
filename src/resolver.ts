a0ecc21393dc9118d4f2ef57a0a8b78f975180d4
Adam Talbot <adamtalbot93@googlemail.com>
2017-02-19 12:41:58 +0000

    Fix resolver implementation

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,20 +1,44 @@
-export class Resolver<T> extends Promise<T> {
+// Taken from http://stackoverflow.com/a/40356701
+export class Resolver<T> implements PromiseLike<T> {
+    private promise: Promise<T>;
+    private resolveFunction: (value?: T | PromiseLike<T>) => void;
+    private rejectFunction: (reason?: any) => void;
 
-    public resolve : (value?: T | PromiseLike<T>) => void
-    
-    public reject : (reason: any) => void
+    constructor(promise?: Promise<T>) {
+        if (!(this instanceof Resolver)){
+            return new Resolver(promise);
+        }
 
-    constructor() {
-        let resolve : (value?: T | PromiseLike<T>) => void
-        let reject : (reason: any) => void
+        this.promise = promise || new Promise(this.promiseExecutor.bind(this));
+    }
 
-        super((res, rej) => {
-            resolve = res;
-            reject = rej;
-        });
+    public asPromise(): Promise<T> {
+        return this.promise;
+    }
 
-        this.resolve = resolve;
-        this.reject = reject;
+    public then<TResult>(onfulfilled?: (value: T) => TResult | PromiseLike<TResult>, onrejected?: (reason: any) => TResult | PromiseLike<TResult>): Resolver<TResult>;
+    public then<TResult>(onfulfilled?: (value: T) => TResult | PromiseLike<TResult>, onrejected?: (reason: any) => void): Resolver<TResult>;
+    public then<TResult>(onfulfilled?: (value: T) => TResult | PromiseLike<TResult>, onrejected?: (reason: any) => any): Resolver<TResult> {
+        return new Resolver(this.promise.then(onfulfilled, onrejected));
+    }
+
+    public catch(onrejected?: (reason: any) => T | PromiseLike<T>): Resolver<T>;
+    public catch(onrejected?: (reason: any) => void): Resolver<T>;
+    public catch(onrejected?: (reason: any) => any): Resolver<T> {
+        return new Resolver(this.promise.catch(onrejected));
+    }
+
+    public resolve(value?: T | PromiseLike<T>) {
+        this.resolveFunction(value);
+    }
+
+    public reject(reason?: any) {
+        this.rejectFunction(reason);
+    }
+
+    private promiseExecutor(resolve: (value?: T | PromiseLike<T>) => void, reject: (reason?: any) => void) {
+        this.resolveFunction = resolve;
+        this.rejectFunction = reject;
     }
 }
 
